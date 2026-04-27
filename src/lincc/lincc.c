4c1d60e876e9e66a23e432a8825e1edf45f1e19d
MONIKAS STOUDIAS FuchsuaProject FrameDOS Project <fuchsuaproject@hotmail.com>
2026-04-27 18:51:03 +0800

    lincc.c

--- src/lincc/lincc.c
+++ src/lincc/lincc.c
@@ -82,16 +82,21 @@ char* bufferOut = NULL;
 struct{
     char* bufferScanedLineOfSource;
     token_t* bufferScanedToken;
-    char** bufferIdent;
+    char* bufferIdent;
     int buffsize[3];
 }buffers = {NULL, NULL, NULL, {0, 0, 0}};
 
+char tempoTokens = NULL{}
 char tempoB[16] = {0,};
 
 void laxer();
+void tokenClass(); /*Sort out a classfication of the tokens*/
 void buffer();
 void paser();
 void allFree();
+
+void addLog(log_t _log_type, int _linenum, errtype_t _error_type);
+void printLogpoint(log_t _log_type);
 
 void main(int argc, char* argv){
     for(int i = 0; i < argc, i++){
@@ -144,36 +149,57 @@ void buffer(){
 1. isspace code sangwi e op bieonsu chuga
 2. gag op giengu mada if mun chuga*/
 
+void tokenClass(const char* tokenString){
+    
+}
+
 void laxer(){
+    int* token_count = *(buffers.buffsize + 2);
     int count = 0;
     int op = 0;
     int opc = 0;
+    char c = bufferIn[count];
+    char next = bufferIn[count+1];
     /*1=keyword, 2=string, 3=num*/
     while(bufferIn[count]){
-	char c = bufferIn[count];
-	char next = bufferIn[count+1];
+	if(*(buffers.bufferScanedToken + *token_count - 1) == NULL){
+	    break;
+	}
 	if(isspace(c) == 1){
 	    switch(op){
 		case 0:
+		    realloc(buffers.bufferScanedToken, sizeof(token_t) * *token_count);
+		    *(buffers.bufferScanedToken + *token_count) = tokenClass(tempoB);
+		    *token_count++;
+		    /*j*/
 		    op = 0;
 		    opc = 0;
-		    continue;
+		    break;
 		case 1:
+		    realloc(buffers.bufferScanedToken, sizeof(token_t) * *token_count);
+		    *(buffers.bufferScanedToken + *token_count) = tokenClass(tempoB);
+		    *token_count++;
 		    op = 0;
 		    opc = 0;
-		    continue;
+		    break;
 		case 2:
+		    tempoB[opc] = bufferIn[count];
 		    op = 2;
-		    opc = 0;
-		    continue;
+		    break;
 		case 3:
+		    realloc(buffers.bufferScanedToken, sizeof(token_t) * *token_count);
+		    *(buffers.bufferScanedToken + *token_count) = tokenClass(tempoB);
+		    *token_count++;
 		    op = 0;
 		    opc = 0;
-		    continue;
+		    break;
 		default:
+		    realloc(buffers.bufferScanedToken, sizeof(token_t) * *token_count);
+		    *(buffers.bufferScanedToken + *token_count) = tokenClass(tempoB);
+		    *token_count++;
 		    op = 0;
 		    opc = 0
-		    continue;
+		    break;
 	    }
 	}
 	else if(isalnum(c) || c == '_'){
@@ -182,17 +208,106 @@ void laxer(){
 		    tempoB[opc] = bufferIn[count];
 		    op = 3;
 		}
+		else if(isalpha(c) || c == '_'){
+		    tempoB[opc] = bufferIn[count];
+		    op = 1;
+		}
 	    }
-	    if(op == 1){
+	    else if(op == 1){
 		tempoB[opc] = bufferIn[count];
-		op = 0;
+		op = 1;
 	    }
+	    else if(op == 2){
+		tempoB[opc] = bufferIn[count];
+	    }
+	    else if(op == 3){
+		if(isnum(c)){
+		    addLog(ERROR, *(buffers.buffsize + 1), _IDENT_AFT_NUM);
+		    break;
+		}
+	    }
+	}
 	else if(c == '\'' || c == '\"'){
 	    if(op == 0){
 		tempoB[opc] = bufferIn[count];
 		op == 2;
 	    }
+	    else if(op == 1){
+		addLog(ERROR, *(buffers.buffsize + 1), _STR_AFT_IDENT);
+		break;
+	    }
+	    else if(op == 2){
+		tempoB[opc] = bufferIn[count];
+		realloc(buffers.bufferScanedToken, sizeof(token_t) * *token_count);
+		*token_count++
+		*(buffers.bufferScanedToken + *token_count) = tokenClass(tempoB);
+		op == 0;
+		opc = 0;
+	    }
+	    else if(op == 3){
+		addLog(ERROR, *(buffers.buffsize + 1), _STR_AFT_NUM);
+		break;
+	    }
 	}
+	else if(c == '\\'){
+	    if(next == 'n'){
+		if(op == 0){
+		    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+		    break;
+		}
+		else(op == 1){
+		    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+		    break;
+		}
+		else if(op == 2){
+		    tempoB[opc] = bufferIn[count];
+		    tempoB[opc + 1] = bufferIn[count + 1]
+		    count++;
+		}
+		else if(op == 3){
+		    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+		    break;
+		}
+	    }
+	    if(next == 't'){
+                if(op == 0){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+                else(op == 1){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+                else if(op == 2){
+                    tempoB[opc] = bufferIn[count];
+                    tempoB[opc + 1] = bufferIn[count + 1]
+                    count++;
+                }
+                else if(op == 3){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+            }
+	    if(next == '\\'){
+                if(op == 0){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+                else(op == 1){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+                else if(op == 2){
+                    tempoB[opc] = bufferIn[count];
+                    tempoB[opc + 1] = bufferIn[count + 1]
+                    count++;
+                }
+                else if(op == 3){
+                    addLog(ERROR, *(buffers.buffsize + 1), _NO_OP_2_LN);
+                    break;
+                }
+            }
+    count++;
     }
 }
 
